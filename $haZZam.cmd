089b0e333405aff141748c119dbf9c125a9629a0
joshuacline <114229084+joshuacline@users.noreply.github.com>
2022-12-09 15:03:16 -0700

    Update $haZZam.cmd

--- $haZZam.cmd
+++ $haZZam.cmd
@@ -1426,16 +1426,17 @@ REM PART_CREATE_PART_CREATE_PART_CREATE_PART_CREATE_PART_CREATE_PART_CREATE
 REM PART_CREATE_PART_CREATE_PART_CREATE_PART_CREATE_PART_CREATE_PART_CREATE
 CALL:PART_UNMOUNT
 CALL:DISKMGR_ERASE
-SET "DEPLOY_MODE="&&SET /A PART_CNT+=1
+SET /A PART_CNT+=1
 (ECHO.select disk %DISK_NUMBER%&&ECHO.create partition EFI size=1024&&ECHO.format quick fs=fat32 label="ESP"&&ECHO.assign letter=Q noerr&&ECHO.create partition primary&&ECHO.format quick fs=ntfs&&ECHO.Exit)>"%TEMP%\DISK_PART"&&DISKPART /s "%TEMP%\DISK_PART"
 (ECHO.select disk %DISK_NUMBER%&&ECHO.select partition 2&&ECHO.assign letter=S noerr&&ECHO.Exit)>"%TEMP%\DISK_PART"&&DISKPART /s "%TEMP%\DISK_PART"
 IF EXIST "Q:\" IF EXIST "S:\" EXIT /B
 CALL:DISKMGR_ERASE
-SET "DEPLOY_MODE=1"&&SET "BOOT_MSG=Drive incompatible, reverted to deploy-only drive."
+IF "%PART_CNT%" EQU "3" SET "DEPLOY_MODE=1"&&SET "BOOT_MSG=Drive incompatible, reverted to deploy-only drive."
 (ECHO.select disk %DISK_NUMBER%&&ECHO.create partition primary size=1024&&ECHO.format quick fs=fat32 label="ESP"&&ECHO.assign letter=Q noerr&&ECHO.create partition primary&&ECHO.format quick fs=ntfs&&ECHO.Exit)>"%TEMP%\DISK_PART"&&DISKPART /s "%TEMP%\DISK_PART"
+IF "%PART_CNT%" LEQ "2" (ECHO.select disk %DISK_NUMBER%&&ECHO.select partition 1&&ECHO.set id=c12a7328-f81f-11d2-ba4b-00a0c93ec93b override&&ECHO.Exit)>"%TEMP%\DISK_PART"&&DISKPART /s "%TEMP%\DISK_PART"
 (ECHO.select disk %DISK_NUMBER%&&ECHO.select partition 2&&ECHO.assign letter=S noerr&&ECHO.Exit)>"%TEMP%\DISK_PART"&&DISKPART /s "%TEMP%\DISK_PART"
 IF EXIST "Q:\" IF EXIST "S:\" EXIT /B
-IF "%PART_CNT%"=="4" ECHO The drive is currently in use or incompatible. Continue to try?&&ECHO  {Z}Continue {Enter}Abort&&CALL SET "PROMPT_SET=ARBIT123"&&CALL:PROMPT_SET
+IF "%PART_CNT%" EQU "4" ECHO The drive is currently in use or incompatible. Continue to try?&&ECHO  {Z}Continue {Enter}Abort&&CALL SET "PROMPT_SET=ARBIT123"&&CALL:PROMPT_SET
 IF NOT "%ARBIT123%"=="Z" SET "PART_ERR=1"
 IF "%ARBIT123%"=="Z" SET "PART_CNT=0"&&SET "ARBIT123="
 IF DEFINED PART_ERR EXIT /B
